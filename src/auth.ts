426b2f157dbc660c362f8af6b6ebdd18d829b81d
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-02 01:51:07 +0800

    feat: Implement Password/Email Auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,55 @@
+import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
-import github from "next-auth/providers/github";
-import google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import { IUserDoc } from "./database/user.model";
+import { SignInSchema } from "./lib/validations";
 
+import Credentials from "next-auth/providers/credentials";
 import { IAccountDoc } from "./database/account.model";
 import { api } from "./lib/api";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [github, google],
+  providers: [
+    GitHub,
+    Google,
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = SignInSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const { data: existingAccount } = (await api.accounts.getByProvider(
+            email
+          )) as ActionResponse<IAccountDoc>;
+
+          if (!existingAccount) return null;
+
+          const { data: existingUser } = (await api.users.getById(
+            existingAccount.userId.toString()
+          )) as ActionResponse<IUserDoc>;
+
+          if (!existingUser) return null;
+
+          const isValidPassword = await bcrypt.compare(
+            password,
+            existingAccount.password!
+          );
+
+          if (isValidPassword) {
+            return {
+              id: existingUser.id,
+              name: existingUser.name,
+              email: existingUser.email,
+              image: existingUser.image,
+            };
+          }
+        }
+        return null;
+      },
+    }),
+  ],
   callbacks: {
     async session({ session, token }) {
       session.user.id = token.sub as string;

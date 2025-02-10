eab5eda9dc0b57e8cb9946824165b98f76d8cc97
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-02-10 23:30:31 +0800

    feat: fetch real question details

--- src/lib/actions/question.action.ts
+++ src/lib/actions/question.action.ts
@@ -5,7 +5,6 @@ import mongoose, { FilterQuery } from "mongoose";
 import Question, { IQuestionDoc } from "@/database/question.model";
 import TagQuestion from "@/database/tag-question.model";
 import Tag, { ITagDoc } from "@/database/tag.model";
-
 import action from "@/lib/handlers/action";
 import handleError from "@/lib/handlers/error";
 import {
@@ -205,7 +204,9 @@ export async function getQuestion(
   const { questionId } = validationResult.params!;
 
   try {
-    const question = await Question.findById(questionId).populate("tags");
+    const question = await Question.findById(questionId)
+      .populate("tags")
+      .populate("author", "_id name image");
 
     if (!question) {
       throw new Error("Question not found");

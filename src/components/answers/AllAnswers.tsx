bbf6695b312010915bc908158e63ea160e858de2
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-25 00:00:03 +0800

    feat: Implement Pagination

--- src/components/answers/AllAnswers.tsx
+++ src/components/answers/AllAnswers.tsx
@@ -3,12 +3,22 @@ import DataRenderer from "@/components/DataRenderer";
 import { AnswerFilters } from "@/constants/filters";
 import { EMPTY_ANSWERS } from "@/constants/states";
 import CommonFilter from "../filters/CommonFilter";
+import Pagination from "../Pagination";
 
 interface Props extends ActionResponse<Answer[]> {
+  page: number;
+  isNext: boolean;
   totalAnswers: number;
 }
 
-const AllAnswers = ({ data, success, error, totalAnswers }: Props) => {
+const AllAnswers = ({
+  data,
+  success,
+  error,
+  totalAnswers,
+  page,
+  isNext,
+}: Props) => {
   return (
     <div className="mt-11">
       <div className="flex items-center justify-between">
@@ -32,6 +42,8 @@ const AllAnswers = ({ data, success, error, totalAnswers }: Props) => {
           answers.map((answer) => <AnswerCard key={answer._id} {...answer} />)
         }
       />
+
+      <Pagination page={page} isNext={isNext || false} />
     </div>
   );
 };
